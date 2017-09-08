43508804f773913500c76949e3524b0db38c8ddc
Ahmed <destiny_hacker@hotmail.com>
2017-09-08 21:47:49 +0200

    started project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+  filename: "index.css"
+})
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+    },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+
+  ]
+  },
+  plugins: [
+        extractSass
+    ]
+
+};
